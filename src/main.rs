fc61dcb6da78ca401452eaee2520b27a61b67008
David Lang <davidlang42@gmail.com>
2023-11-15 21:58:05 +1100

    Add setup notes and example code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod meter;
 
+use rpi_led_matrix::{LedMatrix, LedColor};
+
 fn main() {
-    println!("Hello, world!");
+    // rpi_led_matrix example code
+    let matrix = LedMatrix::new(None, None).unwrap();
+    let mut canvas = matrix.offscreen_canvas();
+    for red in 0..255 {
+        for green in 0..255 {
+            for blue in 0..255 {
+                canvas.fill(&LedColor { red, green, blue });
+                canvas = matrix.swap(canvas);
+            }
+        }
+    }
 }

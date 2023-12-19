bffad1bc5295d48d074fa828c3e707c1cc451051
David Lang <davidlang42@gmail.com>
2023-12-19 22:36:33 +1100

    Shitty clock

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 mod meter;
 
-use rpi_led_matrix::{LedMatrix, LedColor};
+use rpi_led_matrix::{LedMatrix, LedColor, LedFont};
+use chrono::Local;
+use std::path::Path;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
-    // rpi_led_matrix example code
     let matrix = LedMatrix::new(None, None).unwrap();
+    // draw clock while we wait for a midi device
     let mut canvas = matrix.offscreen_canvas();
-    for red in 0..255 {
-        for green in 0..255 {
-            for blue in 0..255 {
-                canvas.fill(&LedColor { red, green, blue });
-                canvas = matrix.swap(canvas);
-            }
-        }
+    let font_path = Path::new("6x9.bdf");
+    let font = LedFont::new(&font_path).unwrap();
+    let color = LedColor { red: 255, green: 255, blue: 255 };
+    loop {
+        let date = Local::now();
+        let time = format!("{}", date.format("%H:%M:%S"));
+        canvas.clear();
+        canvas.draw_text(&font, &time, 0, 0, &color, 0, false);
+        canvas = matrix.swap(canvas);
+        thread::sleep(Duration::from_millis(1000));
+        //TODO check for midi
     }
 }

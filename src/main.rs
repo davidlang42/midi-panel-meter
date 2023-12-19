be48f18251005b2e265040fecd4d8bd6828069ae
David Lang <davidlang42@gmail.com>
2023-12-19 23:05:38 +1100

    Make clock work

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
 mod meter;
 
-use rpi_led_matrix::{LedMatrix, LedColor, LedFont};
+use rpi_led_matrix::{LedMatrix, LedColor, LedFont, LedMatrixOptions};
 use chrono::Local;
 use std::path::Path;
 use std::time::Duration;
 use std::thread;
 
 fn main() {
-    let matrix = LedMatrix::new(None, None).unwrap();
-    // draw clock while we wait for a midi device
+    // set up screen
+    let mut options = LedMatrixOptions::new();
+    options.set_rows(16);
+    options.set_cols(32);
+    let matrix = LedMatrix::new(Some(options), None).unwrap();
+    // draw clock while waiting for midi
     let mut canvas = matrix.offscreen_canvas();
     let font_path = Path::new("6x9.bdf");
     let font = LedFont::new(&font_path).unwrap();
     let color = LedColor { red: 255, green: 255, blue: 255 };
+    let mut colon = true;
     loop {
         let date = Local::now();
-        let time = format!("{}", date.format("%H:%M:%S"));
+        let mut time = if colon {
+            colon = false;
+            format!("{}", date.format("%H:%M"))
+        } else  {
+            colon = true;
+            format!("{}", date.format("%H %M"))
+        };
         canvas.clear();
-        canvas.draw_text(&font, &time, 0, 0, &color, 0, false);
+        canvas.draw_text(&font, &time, 1, 12, &color, 0, false);
         canvas = matrix.swap(canvas);
         thread::sleep(Duration::from_millis(1000));
         //TODO check for midi

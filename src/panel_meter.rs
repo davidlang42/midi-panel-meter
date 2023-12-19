58f4ba6b9c5bfa35aab36641d0810ab61f875607
David Lang <davidlang42@gmail.com>
2023-12-20 01:47:58 +1100

    Draw lines

--- src/panel_meter.rs
+++ src/panel_meter.rs
@@ -45,17 +45,59 @@ impl PanelMeter {
         }
     }
 
-    const WHITE: LedColor = LedColor { red: 255, green: 255, blue: 255 };
+    const FLASH: LedColor = LedColor { red: 255, green: 255, blue: 255 };
+    const CH1: LedColor = LedColor { red: 255, green: 0, blue: 0 };
+    const CH2: LedColor = LedColor { red: 0, green: 255, blue: 0 };
+    const CH3: LedColor = LedColor { red: 0, green: 0, blue: 255 };
 
     pub fn draw(&self, canvas: &mut LedCanvas) {
         canvas.clear();
+        // LHS expression pedal
+        Self::draw_value(canvas, self.expression_cc[0], 0, &Self::CH1);
+        Self::draw_value(canvas, self.expression_cc[1], 1, &Self::CH2);
+        Self::draw_value(canvas, self.expression_cc[2], 2, &Self::CH3);
+        // LHS volume pedal
+        Self::draw_value(canvas, self.volume_cc[0], 4, &Self::CH1);
+        Self::draw_value(canvas, self.volume_cc[1], 5, &Self::CH2);
+        Self::draw_value(canvas, self.volume_cc[2], 6, &Self::CH3);
+        // notes in the middle
+        //TODO
+        // top right corner flash on beat
         if self.tick < 6 {
             for x in 29..32 {
-                for y in 0..3 {
-                    canvas.set(x, y, &Self::WHITE);
-                }
+                canvas.draw_line(x, 0, x, 2, &Self::FLASH);
             }
         }
-        //TODO
+        // RHS damper pedal
+        Self::draw_bool(canvas, self.damper_cc[0], 29, &Self::CH1);
+        Self::draw_bool(canvas, self.damper_cc[1], 30, &Self::CH2);
+        Self::draw_bool(canvas, self.damper_cc[2], 31, &Self::CH3);
+    }
+
+    fn draw_bool(canvas: &mut LedCanvas, b: bool, x: i32, color: &LedColor) {
+        if b {
+            canvas.draw_line(x, 4, x, 15, color)
+        }
+    }
+
+    fn draw_value(canvas: &mut LedCanvas, value: U7, x: i32, color: &LedColor) {
+        let v: u8 = value.into();
+        if v == 127 {
+            canvas.draw_line(x, 0, x, 15, color);
+        } else {
+            let full_pixels = v / 8;
+            let last_pixel = v % 8 * 32;
+            if full_pixels > 0 {
+                canvas.draw_line(x, 16 - full_pixels, x, 15, color)
+            }
+            if last_pixel > 0 {
+                let last_color = LedColor {
+                    red: color.red as usize * last_pixel / 256,
+                    green: color.green as usize * last_pixel / 256,
+                    blue: color.blue as usize * last_pixel / 256
+                };
+                canvas.set(x, 15 - full_pixels, &last_color)
+            }
+        }
     }
 }

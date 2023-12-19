670a3e3822fa5271f9e39f469e4d476acba7415d
David Lang <davidlang42@gmail.com>
2023-12-19 23:29:29 +1100

    Check for midi

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ mod meter;
 
 use rpi_led_matrix::{LedMatrix, LedColor, LedFont, LedMatrixOptions};
 use chrono::Local;
+use std::ops::AddAssign;
 use std::path::Path;
 use std::time::Duration;
 use std::thread;
+use std::fs;
+use std::error::Error;
+use std::time::Instant;
 
 fn main() {
     // set up screen
@@ -19,18 +23,43 @@ fn main() {
     let color = LedColor { red: 255, green: 255, blue: 255 };
     let mut colon = true;
     loop {
-        let date = Local::now();
-        let mut time = if colon {
+        let updated = Instant::now();
+        let time = if colon {
             colon = false;
-            format!("{}", date.format("%H:%M"))
+            format!("{}", Local::now().format("%H:%M"))
         } else  {
             colon = true;
-            format!("{}", date.format("%H %M"))
+            format!("{}", Local::now().format("%H %M"))
         };
         canvas.clear();
         canvas.draw_text(&font, &time, 1, 11, &color, 0, false);
         canvas = matrix.swap(canvas);
-        thread::sleep(Duration::from_millis(1000));
-        //TODO check for midi
+        if let Some(device) = list_files("/dev", "midi").unwrap().into_iter().next() {
+            canvas.clear();
+            canvas.draw_text(&font, &device, 1, 11, &color, 0, false);
+            canvas = matrix.swap(canvas);
+            thread::sleep(Duration::from_millis(3000));
+        }
+        let ms = updated.elapsed().as_millis();
+        if ms < 1000 {
+            thread::sleep(Duration::from_millis((1000 - ms).try_into().unwrap()));
+        }
+    }
+}
+
+fn list_files(root: &str, prefix: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let md = fs::metadata(root)?;
+    if md.is_dir() {
+        let mut files = Vec::new();
+        for entry in fs::read_dir(root)? {
+            let path = entry?.path();
+            if !path.is_dir() && path.file_name().unwrap().to_string_lossy().starts_with(prefix) {
+                files.push(path.display().to_string());
+            }
+        }
+        files.sort();
+        Ok(files)
+    } else {
+        Ok(vec![root.to_string()])
     }
 }
